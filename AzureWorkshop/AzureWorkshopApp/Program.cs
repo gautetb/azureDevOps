d07b36ccb1fb71061c2551f1821d9ff4bf7bb058
Gaute Tessand Baalsrud <gaute.baalsrud@bouvet.no>
2019-08-19 13:24:24 +0200

    first

---
+++ AzureWorkshop/AzureWorkshopApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
+namespace AzureWorkshopApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private const string KeyVaultEndpoint = "https://gautekey.vault.azure.net/";
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .ConfigureAppConfiguration((ctx, builder) =>
+                   {
+                       var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                       var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                       builder.AddAzureKeyVault(KeyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                   })
+                   .UseStartup<Startup>();
+    }
+}
---
+++ AzureWorkshop/AzureWorkshopApp/Services/StorageService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using AzureWorkshopApp.Helpers;
+using AzureWorkshopApp.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureWorkshopApp.Services
+{
+    public class StorageService : IStorageService
+    {
+        private readonly AzureStorageConfig _storageConfig;
+
+        public object BlobContinuationToken { get; private set; }
+
+        public StorageService(IOptions<AzureStorageConfig> storageConfig)
+        {
+            _storageConfig = storageConfig != null ? storageConfig.Value : throw new ArgumentNullException(nameof(storageConfig));
+        }
+
+        public AzureStorageConfigValidationResult ValidateConfiguration()
+        {
+            return StorageConfigValidator.Validate(_storageConfig);
+        }
+
+        public async Task<bool> UploadFileToStorage(Stream fileStream, string fileName)
+        {
+            var storageCred = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+            var blobstorage = new CloudStorageAccount(storageCred, true);
+            var cloudBlob = blobstorage.CreateCloudBlobClient();
+            var container = cloudBlob.GetContainerReference(_storageConfig.ImageContainer);
+            var blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadFromStreamAsync(fileStream);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<List<string>> GetImageUrls()
+        {
+            List<string> imageUrls = new List<string>();
+
+            var storageCred = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+            var blobstorage = new CloudStorageAccount(storageCred, true);
+            var cloudBlob = blobstorage.CreateCloudBlobClient();
+            var container = cloudBlob.GetContainerReference(_storageConfig.ImageContainer);
+
+            BlobContinuationToken continuationToken = null;
+
+            //Call ListBlobsSegmentedAsync and enumerate the result segment returned, while the continuation token is non-null.
+            //When the continuation token is null, the last page has been returned and execution can exit the loop.
+            do
+            {
+                //This overload allows control of the page size. You can return all remaining results by passing null for the maxResults parameter,
+                //or by calling a different overload.
+                BlobResultSegment resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
+
+                foreach (var blobItem in resultSegment.Results)
+                {
+                    imageUrls.Add(blobItem.StorageUri.PrimaryUri.ToString());
+                }
+
+                //Get the continuation token.
+                continuationToken = resultSegment.ContinuationToken;
+            }
+
+            while (continuationToken != null);
+
+            return await Task.FromResult(imageUrls);
+        }
+    }
+}
